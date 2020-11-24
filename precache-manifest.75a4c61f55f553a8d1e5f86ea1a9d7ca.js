df087886fccb6ceaebf1c0c04b1de34fbee924b5
Max Rebedailo <akrosom21@gmail.com>
2020-11-24 18:26:28 +0200

    Updates

---
+++ precache-manifest.75a4c61f55f553a8d1e5f86ea1a9d7ca.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf7586fa7eda593d74f3a728bcb8ed7",
+    "url": "/react-start/index.html"
+  },
+  {
+    "revision": "42ad596f13806e1a94e5",
+    "url": "/react-start/static/css/3.069d1ce7.chunk.css"
+  },
+  {
+    "revision": "77eb2c8577231565892f",
+    "url": "/react-start/static/css/main.c41bd5eb.chunk.css"
+  },
+  {
+    "revision": "b37408cc697954666431",
+    "url": "/react-start/static/js/2.b9bfd5a5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-start/static/js/2.b9bfd5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42ad596f13806e1a94e5",
+    "url": "/react-start/static/js/3.fd0088ac.chunk.js"
+  },
+  {
+    "revision": "77eb2c8577231565892f",
+    "url": "/react-start/static/js/main.1b75a5ba.chunk.js"
+  },
+  {
+    "revision": "e41d9743dac842334ad5",
+    "url": "/react-start/static/js/runtime-main.fe874462.js"
+  },
+  {
+    "revision": "4a9ff1920f1e76a97189f61c8662fc94",
+    "url": "/react-start/static/media/ava.4a9ff192.jfif"
+  },
+  {
+    "revision": "88d71391b7f7c1dd206cbf89b8ecbb3d",
+    "url": "/react-start/static/media/ava002.88d71391.jfif"
+  },
+  {
+    "revision": "47a5f3394ea0f4d137675c27ebaab569",
+    "url": "/react-start/static/media/logo.47a5f339.webp"
+  },
+  {
+    "revision": "27fc27b682d7147fc0935299c8467922",
+    "url": "/react-start/static/media/preloader.27fc27b6.svg"
+  },
+  {
+    "revision": "180cda3a11db2494808033e4f46d2d68",
+    "url": "/react-start/static/media/profile_bg.180cda3a.webp"
+  }
+]);

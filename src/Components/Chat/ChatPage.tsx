5163e8d2c767e35448e370d32ac3c45524894710
Max Rebedailo <akrosom21@gmail.com>
2021-02-07 23:01:47 +0200

    add removeEventListeners and closing of websocket before unmount of component

--- src/Components/Chat/ChatPage.tsx
+++ src/Components/Chat/ChatPage.tsx
@@ -2,9 +2,8 @@ import React, {useEffect, useState} from "react";
 import {Chat} from "./Chat";
 import {SendingForm} from "./SendingForm";
 
-const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-
 function ChatPage() {
+    const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
     type ChatMessagesType = {
         message: string,
         photo: string,
@@ -12,22 +11,42 @@ function ChatPage() {
         userName: string
     }
     const [messages, setMessages] = useState<ChatMessagesType[]>([])
+    //get messages
     useEffect(() => {
-        ws.addEventListener('message', (e) => {
+        const getMessages = (e) => {
             let newMessages = JSON.parse(e.data)
             setMessages((prevMessages) => [...prevMessages, ...newMessages])
-        })
+        }
+        ws.addEventListener('message', getMessages)
+
+        return ()=> {
+            ws.removeEventListener('message', getMessages)
+            ws.close()
+        }
     }, [])
+
+    //send new message
     const onAddMessage = (message) => {
         ws.send(message)
     }
+
     //Disable button till websocket will be ready
     const [readyStatus, setReadyStatus] = useState('')
     useEffect(() => {
-         ws.addEventListener('open', () => {
-             setReadyStatus('ready')
-         })
-    })
+        function changeStatus() {
+            setReadyStatus('ready')
+        }
+         ws.addEventListener('open', changeStatus)
+
+        return () => {
+            ws.removeEventListener('open', changeStatus)
+        }
+    }, [])
+
+    //alert if connection was lost
+    useEffect(()=> {
+        ws.addEventListener('close', ()=> alert('connection was lost'))
+    }, [])
     return (
         <div>
             <Chat messages={messages}/>

d71165efd7f9d51b9cf3bf47f9a54a85b7888402
Max Rebedailo <akrosom21@gmail.com>
2021-02-13 16:49:49 +0200

    add autoscrolling to chat

--- src/Components/Chat/Chat.tsx
+++ src/Components/Chat/Chat.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import classes from '../Chat/Chat.module.css'
 
 export const Chat = (props) => {
+    const anchorRef = useRef(document.createElement("div"))
+    const [isAutoScroll, setIsAutoScroll] = useState(true)
+    const onScrollHandler = (e) => {
+        const element = e.currentTarget
+        if (Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) <300 ) {
+            setIsAutoScroll(true)
+        } else {
+            setIsAutoScroll(false)
+        }
+    }
+    useEffect(() => {
+        if (isAutoScroll) {
+            anchorRef.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [props.messages])
     return (
-        <div className={classes.chat}>
+        <div className={classes.chat} onScroll={onScrollHandler}>
             {props.messages.map((message, index) => {
                 return <div className={classes.chat__inner} key={index}>
                     <div className={classes.chat__user}>
@@ -13,6 +28,7 @@ export const Chat = (props) => {
                     <div className={classes.chat__text}>{message.message}</div>
                 </div>
             })}
+            <div ref={anchorRef}></div>
         </div>
     )
 }
